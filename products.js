4cce63e362780310efa8eff5e790d44ca9dc1db1
woodtop <cwood89@mac.com>
2020-04-16 13:41:35 -0400

    created product template and added to html

--- products.js
+++ products.js
@@ -20,7 +20,38 @@ let products = [
 ];
 
 $( document ).ready(function() {
+  let row1 = $("#row-1")
+  let row2 = $("#row-2")
+  let row3 = $("#row-3")
+  products.forEach(function (product, index) {
+    let item = `<div class="col-md-3 col-sm-6">
+    <div class="product-grid6">
+      <div class="product-image6">
+        <a href="#">
+          <img class="pic-1" src="${product.image}">
+        </a>
+      </div>
+      <div class="product-content">
+        <h3 class="title"><a href="#">${product.name}</a></h3>
+        <div class="price">
+        ${product.price}
+        </div>
+      </div>
+      <ul class="social">
+        <li><a href="" data-tip="Quick View"><i class="fa fa-search"></i></a></li>
+        <li><a href="" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+      </ul>
+    </div>
+  </div>`
   
+    if (index + 1 <= 4) {
+      row1.append(item)
+    } else if (index  + 1 > 4 && index + 1 <= 8) {
+      row2.append(item)
+    } else if (index + 1 > 8 ) {
+      row3.append(item)
+    }
+  })
 });
 /*
 <div class="col-md-3 col-sm-6">
