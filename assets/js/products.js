38ec1021440ce4081e7cc3604be26085449e10d5
woodtop <cwood89@mac.com>
2020-04-21 14:09:43 -0400

    added modal functionally

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,8 +1,10 @@
-function Product(name, popularity, image) {
+function Product(name, popularity, image, description) {
+  this.id = name.replace(/\s/g, '');
   this.name = name;
   this.price = 7.99;
   this.popularity = popularity; // 1-10 scale
   this.image = image;
+  this.description = description
 }
 let products = [
   new Product("Super Berry", 6, "images/superberry.png"),
@@ -28,28 +30,57 @@ let products = [
         <h4>${product.name}</h4>
         <p>${product.price}</p>
         <div class="portfolio-links">
-          <a href="assets/img/portfolio/portfolio-1.jpg" data-gall="portfolioGallery" class="venobox" title="Add to Cart"><i class="bx bx-plus"></i></a>
-          <a  data-toggle="modal" data-target="#${product.name}"title="More Details"><i class="bx bx-link"></i></a>
+          <a href="#" title="Add to Cart"><i class="bx bx-plus"></i></a>
+          <a  data-toggle="modal" data-target="#${product.id}"title="More Details"><i class="bx bx-link"></i></a>
         </div>
       </div>
     </div>
   </div>
   
-  <div class="modal fade" id="${product.name}" tabindex="-1" role="dialog" aria-labelledby="${product.name}modal" aria-hidden="true">
+  <div class="modal fade" id="${product.id}" tabindex="-1" role="dialog" aria-labelledby="${product.id}modal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLongTitle">${product.name}</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          ...
+       
+        <!-- ======= Portfolio Details Section ======= -->
+        <section id="portfolio-details" class="portfolio-details">
+          <div class="container">
+    
+            <div class="row">
+              <div class="col-lg-8">
+                <img src="${product.image}"class="img-fluid" alt="">
+              </div>
+    
+              <div class="col-lg-4 portfolio-info">
+                <h3>Product Information</h3>
+                <ul>
+                  <li><strong>Category</strong>: Web design</li>
+                  <li><strong>Client</strong>: ASU Company</li>
+                  <li><strong>Project date</strong>: 01 March, 2020</li>
+                  <li><strong>Project URL</strong>: <a href="#">www.example.com</a></li>
+                </ul>
+                </div>
+                <div class="row">
+                  <div class="container">
+                    <p>
+                    Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.
+                    </p>
+                    </div>
+                </div>
+    
+            </div>
+    
+          </div>
+        </section><!-- End Portfolio Details Section -->
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button"  id="modal-button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
